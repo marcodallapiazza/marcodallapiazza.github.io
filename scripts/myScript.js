1852cdeba54b1995b746ece6908ea55fca226136
marcodallapiazza <marco.dallapiazza@gmail.com>
2016-10-14 09:54:04 +0200

    structure

---
+++ scripts/myScript.js
@@ -0,0 +1,28 @@
+﻿var widthSito = 0;
+var heightSito = 0;
+
+$(document).ready(function () {
+    SetLayout();
+});
+
+$(window).resize(function () {
+    SetLayout();
+});
+
+function SetLayout() {
+    widthSito = $(window).width();
+    heightSito = $(window).height();
+
+    if (widthSito <= 900) {
+        if (!$('#master').hasClass('size-320')) {
+            $('#master').removeClass().addClass('size-320');
+        }
+    } else {
+        if (!$('#master').hasClass('size-normal')) {
+            $('#master').removeClass().addClass('size-normal');
+        }
+    }
+
+    // pagina
+    $('body').css('visibility', 'visible');
+}
