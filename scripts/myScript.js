627b3c833453996006d131169091cf7d7b61565d
marcodallapiazza <marco.dallapiazza@gmail.com>
2017-02-10 12:23:53 +0100

    news

--- scripts/myScript.js
+++ scripts/myScript.js
@@ -1,31 +1,3 @@
-﻿var widthSito = 0;
-var heightSito = 0;
+﻿$(document).ready(function () {
 
-$(document).ready(function () {
-    SetLayout();
-
-    $('#header').addClass('translate');
-    $('#content').addClass('translate');
 });
-
-$(window).resize(function () {
-    SetLayout();
-});
-
-function SetLayout() {
-    widthSito = $(window).width();
-    heightSito = $(window).height();
-
-    if (widthSito <= 900) {
-        if (!$('#master').hasClass('size-320')) {
-            $('#master').removeClass().addClass('size-320');
-        }
-    } else {
-        if (!$('#master').hasClass('size-normal')) {
-            $('#master').removeClass().addClass('size-normal');
-        }
-    }
-
-    // pagina
-    $('body').css('visibility', 'visible');
-}
